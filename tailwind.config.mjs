616519b5f6de2ae949cb14152f6f6ab5eb2c6fea
dathoangquoc <dathgqc@gmail.com>
2024-06-24 10:50:28 +0700

    Tuyen Dung

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,24 +2,6 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-
-		screens: {
-			'sm': '540px',
-			// => @media (min-width: 640px) { ... }
-
-			'md': '720px',
-			// => @media (min-width: 768px) { ... }
-
-			'lg': '960px',
-			// => @media (min-width: 1024px) { ... }
-
-			'xl': '1140px',
-			// => @media (min-width: 1280px) { ... }
-
-			'2xl': '1320px',
-			// => @media (min-width: 1536px) { ... }
-		},
-
 		container: {
 			center: true,
 		},
